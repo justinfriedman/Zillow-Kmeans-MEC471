d76a15f7aa661322fc425c991fc8e9b0cf2cdbc5
justin friedman <justinfriedman22@gmail.com>
2017-12-04 11:28:12 -0600

    classified test

--- data/utils/classifier.py
+++ data/utils/classifier.py
@@ -18,44 +18,44 @@ for feature in dfOrigCols:
 
     df[name +'_AVE_C1'] = df[name][df['_clus_k5'] == 1].mean()
     count = count + 1
-    sys.stdout.write('on feature ' + str(count) + ' of ' + str(numCols) + '\r')
+    sys.stdout.write('📟 on feature ' + str(count) + ' of ' + str(numCols) + '\r')
     sys.stdout.flush()
 
-print("cluster 1 done, starting cluster 2")
+print("✅ cluster 1 done, starting cluster 2")
 count = 0
 for feature in dfOrigCols:
     name = feature
     df[name +'_AVE_C2'] = pandas.Series(1200, index=df.index)
     df[name +'_AVE_C2']= df[name][df['_clus_k5'] == 2].mean()
     count = count + 1
-    sys.stdout.write('on feature ' + str(count) + ' of ' + str(numCols) + '\r')
+    sys.stdout.write('📟 on feature ' + str(count) + ' of ' + str(numCols) + '\r')
     sys.stdout.flush()
-print("cluster 2 done, starting cluster 3")
+print("✅ cluster 2 done, starting cluster 3")
 count = 0
 for feature in dfOrigCols:
     name = feature
     df[name +'_AVE_C3'] = pandas.Series(1200, index=df.index)
     df[name +'_AVE_C3']= df[name][df['_clus_k5'] == 3].mean()
     count = count + 1
-    sys.stdout.write('on feature ' + str(count) + ' of ' + str(numCols) + '\r')
+    sys.stdout.write('📟 on feature ' + str(count) + ' of ' + str(numCols) + '\r')
     sys.stdout.flush()
-print("cluster 3 done, starting cluster 4")
+print("✅ cluster 3 done, starting cluster 4")
 count = 0
 for feature in dfOrigCols:
     name = feature
     df[name +'_AVE_C4'] = pandas.Series(1200, index=df.index)
     df[name +'_AVE_C4']= df[name][df['_clus_k5'] == 4].mean()
     count = count + 1
-    sys.stdout.write('on feature ' + str(count) + ' of ' + str(numCols) + '\r')
+    sys.stdout.write('📟 on feature ' + str(count) + ' of ' + str(numCols) + '\r')
     sys.stdout.flush()
-print("cluster 4 done, starting cluster 5")
+print("✅ cluster 4 done, starting cluster 5")
 count = 0
 for feature in dfOrigCols:
     name = feature
     df[name +'_AVE_C5'] = pandas.Series(1200, index=df.index)
     df[name +'_AVE_C5']= df[name][df['_clus_k5'] == 5].mean()
     count = count + 1
-    sys.stdout.write('on feature ' + str(count) + ' of ' + str(numCols) + '\r')
+    sys.stdout.write('📟 on feature ' + str(count) + ' of ' + str(numCols) + '\r')
     sys.stdout.flush()
 
 
@@ -73,7 +73,7 @@ print("starting classification")
 #test data
 # dfTestOrig = pandas.read_stata('../ames_test.dta')
 #train data
-dfTestOrig = pandas.read_stata('../ZillowData_realPrice-train.dta')
+dfTestOrig = pandas.read_stata('../test_dataFILTERED_FINAL_11AM.dta')
 dfTestOrigCols = dfTestOrig.columns
 dfTest = dfTestOrig
 
@@ -89,7 +89,7 @@ for row in dfTest.index:
     clusterTally = [0,0,0,0,0]
     colName = ''
     for feature in dfTestOrigCols:
-        sys.stdout.write('on row ' + str(row) + ' and col ' + str(feature) + '\r')
+        sys.stdout.write('📟 on row ' + str(row) + ' and col ' + str(feature) + '\r')
         sys.stdout.flush()
         colName = feature
         val = dfTest.get_value(row, feature, takeable=False)
@@ -102,32 +102,32 @@ for row in dfTest.index:
         # val_residual_cluster5Percent = (df.at[row, feature+'_AVE_C5'] - val)/(df.get_value(row, feature+'_AVE_C5', takeable=False)+ 0.0000000001)
 
         #ln Bruteforce
-        if abs(df.at[row, feature+'_AVE_C1'] - val) != 0:
-            val_residual_cluster1Percent = np.log(abs(df.at[row, feature+'_AVE_C1'] - val))
-        if (df.at[row, feature+'_AVE_C1'] - val) == 0:
+        if abs(df.at[3, feature+'_AVE_C1'] - val) != 0:
+            val_residual_cluster1Percent = np.log(abs(df.at[3, feature+'_AVE_C1'] - val))
+        if (df.at[3, feature+'_AVE_C1'] - val) == 0:
             val_residual_cluster1Percent = 0
 
-        if abs(df.at[row, feature+'_AVE_C2'] - val) != 0:
-            val_residual_cluster2Percent = np.log(abs(df.at[row, feature+'_AVE_C2'] - val))
-        if (df.at[row, feature+'_AVE_C2'] - val) == 0:
+        if abs(df.at[3, feature+'_AVE_C2'] - val) != 0:
+            val_residual_cluster2Percent = np.log(abs(df.at[3, feature+'_AVE_C2'] - val))
+        if (df.at[3, feature+'_AVE_C2'] - val) == 0:
             val_residual_cluster2Percent = 0
 
 
-        if abs(df.at[row, feature+'_AVE_C3'] - val) != 0:
-            val_residual_cluster3Percent = np.log(abs(df.at[row, feature+'_AVE_C3'] - val))
-        if (df.at[row, feature+'_AVE_C3'] - val) == 0:
+        if abs(df.at[3, feature+'_AVE_C3'] - val) != 0:
+            val_residual_cluster3Percent = np.log(abs(df.at[3, feature+'_AVE_C3'] - val))
+        if (df.at[3, feature+'_AVE_C3'] - val) == 0:
             val_residual_cluster3Percent = 0
 
 
-        if abs(df.at[row, feature+'_AVE_C4'] - val) != 0:
-            val_residual_cluster4Percent = np.log(abs(df.at[row, feature+'_AVE_C4'] - val))
-        if (df.at[row, feature+'_AVE_C4'] - val) == 0:
+        if abs(df.at[3, feature+'_AVE_C4'] - val) != 0:
+            val_residual_cluster4Percent = np.log(abs(df.at[3, feature+'_AVE_C4'] - val))
+        if (df.at[3, feature+'_AVE_C4'] - val) == 0:
             val_residual_cluster4Percent = 0
 
 
-        if abs(df.at[row, feature+'_AVE_C5'] - val) != 0:
-            val_residual_cluster5Percent = np.log(abs(df.at[row, feature+'_AVE_C5'] - val))
-        if (df.at[row, feature+'_AVE_C5'] - val) == 0:
+        if abs(df.at[3, feature+'_AVE_C5'] - val) != 0:
+            val_residual_cluster5Percent = np.log(abs(df.at[3, feature+'_AVE_C5'] - val))
+        if (df.at[3, feature+'_AVE_C5'] - val) == 0:
             val_residual_cluster5Percent = 0
 
 
@@ -175,9 +175,9 @@ for row in dfTest.index:
 
 
     #export dta and excel because we couldnt export dta because sometimes python is a globalist cuck like jared kushner
-print("exporting train_output_with_feature_means.dta")
+print("✅ exporting train_output_with_feature_means.dta ⏬")
 df.to_stata("output/train_output_with_feature_means.dta", convert_dates=None, write_index=True, encoding='latin-1', byteorder=None, time_stamp=None, data_label=None, variable_labels=None)
 
 # dfTest.to_stata("output/classified_test_output.dta", convert_dates=None, write_index=True, encoding='latin-1', byteorder=None, time_stamp=None, data_label=None, variable_labels=dfTest.columns)
-print("exporting test_output_classified.xlsx")
+print("✅ exporting test_output_classified.xlsx ⏬")
 dfTest.to_excel("output/test_output_classified.xlsx", sheet_name='Sheet1', na_rep='', float_format=None, columns=None, header=True, index=True, index_label=None, startrow=0, startcol=0, engine=None, merge_cells=True, encoding=None, inf_rep='inf', verbose=True, freeze_panes=None)
